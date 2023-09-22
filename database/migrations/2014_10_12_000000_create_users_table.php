e187eee3274db615e00c3bf993c4c1b1abc7baab
Jude Danbo <judedanbo@gmail.com>
2023-09-22 05:42:52 +0000

    Remove person_id form user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('person_id')->nullable()->constrained()->onUpdate('cascade');
+            // $table->foreignId('person_id')->nullable()->constrained()->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
