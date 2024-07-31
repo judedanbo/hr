c19601208a28ca02f09ba58c0c3251fbc995e804
Jude Danbo <judedanbo@gmail.com>
2024-07-31 15:00:32 +0000

    Added Ghana card to Enum. added Ghana card Column to export excel file

--- app/Enums/Identity.php
+++ app/Enums/Identity.php
@@ -6,8 +6,21 @@ enum Identity: string
 {
     case NOT_AVAILABLE = "";
     case NationalID = 'N';
+    case GhanaCard = 'G';
     case Social_Security_Number = 'S';
     case Passport = 'P';
     case Driver_License = 'D';
     case TIN = 'T';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::NationalID => 'National ID',
+            self::GhanaCard => 'Ghana Card',
+            self::Social_Security_Number => 'Social Security Number',
+            self::Passport => 'Passport',
+            self::Driver_License => 'Driver License',
+            self::TIN => 'Tax Identification Number',
+        };
+    }
 }
--- app/Exports/StaffDetailsExport.php
+++ app/Exports/StaffDetailsExport.php
@@ -2,17 +2,22 @@
 
 namespace App\Exports;
 
+use App\Enums\Identity;
 use App\Models\InstitutionPerson;
 use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Style\Alignment as StyleAlignment;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class StaffDetailsExport implements FromQuery, WithMapping, WithHeadings, ShouldQueue, ShouldAutoSize
+class StaffDetailsExport implements FromQuery, WithMapping, WithHeadings, ShouldQueue, ShouldAutoSize, WithStyles
 {
     use Exportable;
 
@@ -24,6 +29,8 @@ class StaffDetailsExport implements FromQuery, WithMapping, WithHeadings, Should
             'Full Name',
             'Date of Birth',
             'Age',
+            'Ghana Card Number',
+            'Social Security Number',
             'Appointment Date',
             'Years served',
             'Current Rank',
@@ -41,6 +48,8 @@ class StaffDetailsExport implements FromQuery, WithMapping, WithHeadings, Should
             $staff->person->full_name,
             $staff->person->date_of_birth?->format('d F, Y'),
             $staff->person->date_of_birth?->diffInYears() . " years",
+            $staff->person->identities->where('id_type', Identity::GhanaCard)->first()?->id_number,
+            // $staff->person->identities->where('id_type', Identity::Social_Security_Number)->first()?->id_number,
             $staff->hire_date?->format('d F, Y'),
             $staff->hire_date === null ? '' : Carbon::now()->diffInYears($staff->hire_date) . ' years',
             $staff->currentRank?->job?->name,
@@ -51,10 +60,25 @@ class StaffDetailsExport implements FromQuery, WithMapping, WithHeadings, Should
             $staff->currentRank?->job?->category->level ?? null,
         ];
     }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1    => ['font' => ['bold' => true]],
+
+            // Styling a specific cell by coordinate.
+            'F' => ['alignment' => ['horizontal' => StyleAlignment::HORIZONTAL_LEFT]],
+
+            // Styling an entire column.
+            // 'G'  => ['font' => ['size' => 16]],
+        ];
+    }
+
     function query()
     {
         return InstitutionPerson::query()
-            ->with(['person'])
+            ->with(['person.identities'])
             ->currentRank()
             ->currentUnit()
             ->active();
