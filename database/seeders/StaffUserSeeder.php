4a7805ff1aab4cfa2461fc8b9b467531f58c30ba
Jude Danbo <judedanbo@gmail.com>
2025-03-13 12:26:28 +0000

    Staff role and permissions added

---
+++ database/seeders/StaffUserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class StaffUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Permission::create(['name' => 'upload avatar']);
+        Permission::create(['name' => 'edit avatar']);
+        Permission::create(['name' => 'view dependent']);
+        Permission::create(['name' => 'create dependent']);
+        Permission::create(['name' => 'edit dependent']);
+        Permission::create(['name' => 'delete dependent']);
+
+        Role::create(['name' => 'staff'])
+            ->givePermissionTo([
+                'view staff',
+                'upload avatar',
+                'edit avatar',
+                'view dependent',
+                'create dependent',
+                'edit dependent',
+                'delete dependent',
+            ]);
+    }
+}
